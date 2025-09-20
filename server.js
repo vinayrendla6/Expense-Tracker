f39093ed5aab299c59252882e78e5eb5f97d777c
vinayrendla6 <22r01a04c2@cmrithyderabad.edu.in>
2025-09-20 18:59:31 +0530

    Fix: remove client submodule metadata and update configs

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const colors = require("colors");
+const path = require("path");
 const connectDb = require("./config/connectDb");
 // config dot env file
 dotenv.config();
@@ -24,8 +25,18 @@ app.use("/api/v1/users", require("./routes/userRoute"));
 //transection routes
 app.use("/api/v1/transections", require('./routes/transectionRoutes'))
 
+
+//static files
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+})
+
 //port
-const PORT = 8080 || process.env.PORT;
+const PORT = process.env.PORT || 8080;
+
 
 //listen server
 app.listen(PORT, () => {
