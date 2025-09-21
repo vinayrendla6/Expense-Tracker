da94f1316a34959e821c33985a552c7bedd20fe1
vinayrendla6 <22r01a04c2@cmrithyderabad.edu.in>
2025-09-21 13:36:08 +0530

    first-commit

--- server.js
+++ server.js
@@ -30,9 +30,7 @@ app.use("/api/v1/transections", require('./routes/transectionRoutes'))
 app.use(express.static(path.join(__dirname, "./client/build")));
 
 
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-})
+
 
 //port
 const PORT = process.env.PORT || 8080;
